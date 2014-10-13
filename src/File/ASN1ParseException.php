feeb76124ee927435feb6a2ff828ede12e716226
Francis Gonzales <fgonzalestello91@gmail.com>
2014-10-13 12:39:09 -0500

    moving to src

---
+++ src/File/ASN1ParseException.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * ASN1ParseException
+ *
+ * @author  Jim Wigginton <terrafrost@php.net>
+ * @version 0.3.5
+ * @access  public
+ * @package ASN1
+ */
+
+namespace File\ASN1ParseException;
+
+class ASN1ParseException extends Exception {}
+
